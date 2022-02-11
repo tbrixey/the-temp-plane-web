4c7c356ca01ed976e366df13cb5d9a5596469de6
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2022-02-10 23:25:09 -0600

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,17 +3,42 @@
 const MetaTags = () => {
   return (
     <>
-      <meta name="og:url" content="https://thetemporaryplane.com" />
-      <meta name="og:title" content="The Temporary Plane" />
       <meta
-        name="og:description"
-        content="Immerse yourself in an ever-changing and ever-growing game space
-            catered and created by you."
+        name="description"
+        content="Immerse yourself in an ever-changing and ever-growing game space catered and created by you."
       />
-      <meta property="og:image" content="https://i.imgur.com/SEuzdzg.jpg" />
+
+      {/*google*/}
+      <meta itemProp="name" content="The Temporary Plane" />
       <meta
-        property="twitter:image"
-        content="https://i.imgur.com/SEuzdzg.jpg"
+        itemProp="description"
+        content="Immerse yourself in an ever-changing and ever-growing game space catered and created by you."
+      />
+      <meta itemProp="image" content="https://thetemporaryplane.com/map.jpg" />
+
+      {/*facebook*/}
+      <meta property="og:url" content="https://thetemporaryplane.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="The Temporary Plane" />
+      <meta
+        property="og:description"
+        content="Immerse yourself in an ever-changing and ever-growing game space catered and created by you."
+      />
+      <meta
+        property="og:image"
+        content="https://thetemporaryplane.com/map.jpg"
+      />
+
+      {/*twitter*/}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="The Temporary Plane" />
+      <meta
+        name="twitter:description"
+        content="Immerse yourself in an ever-changing and ever-growing game space catered and created by you."
+      />
+      <meta
+        name="twitter:image"
+        content="https://thetemporaryplane.com/map.jpg"
       />
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="600" />
