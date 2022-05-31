d93d2cb15184975bb7622d77c648e3b4d036fa57
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2022-05-31 09:11:38 -0500

    Update newPlayerSetup.tsx

--- components/newPlayerSetup.tsx
+++ components/newPlayerSetup.tsx
@@ -1,4 +1,13 @@
-import { FormControl, Grid, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import { find } from "lodash";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import {
@@ -19,59 +28,106 @@ export const NewPlayerSetup = () => {
   return (
     <>
       {classes && cities && races && (
-        <Grid container flexDirection={"column"}>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Class
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={selectedClass}
-              onChange={(event) => setSelectedClass(event.target.value)}
-              label="Class"
-            >
-              {classes.map((item, idx: number) => (
-                <MenuItem key={item.name + idx} value={item.name}>
-                  {item.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">Race</InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={selectedRace}
-              onChange={(event) => setSelectedRace(event.target.value)}
-              label="Race"
-            >
-              {races.map((item, idx: number) => (
-                <MenuItem key={item.name + idx} value={item.name}>
-                  {item.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Starting Location
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={selectedLocation}
-              onChange={(event) => setSelectedLocation(event.target.value)}
-              label="Starting Location"
-            >
-              {cities.map((item, idx: number) => (
-                <MenuItem key={item.name + idx} value={item._id}>
-                  {item.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+        <Grid
+          container
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Grid item sx={{ maxWidth: 350 }}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+              <InputLabel id="demo-simple-select-standard-label">
+                Class
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={selectedClass}
+                onChange={(event) => setSelectedClass(event.target.value)}
+                label="Class"
+                fullWidth
+              >
+                {classes.map((item, idx: number) => (
+                  <MenuItem key={item.name + idx} value={item.name}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            {!selectedClass && <br />}
+            {selectedClass && (
+              <>
+                <Typography>
+                  {find(classes, { name: selectedClass })?.description}
+                </Typography>
+                <Typography>
+                  <b>Speed: </b> {find(classes, { name: selectedClass })?.speed}{" "}
+                  <b>Weight: </b>{" "}
+                  {find(classes, { name: selectedClass })?.weight}
+                </Typography>
+                <Typography>
+                  <b>Bonus stats:</b>{" "}
+                  {find(classes, { name: selectedClass })?.bonus}
+                </Typography>
+              </>
+            )}
+          </Grid>
+          <Grid item sx={{ maxWidth: 350 }}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+              <InputLabel id="demo-simple-select-standard-label">
+                Race
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={selectedRace}
+                onChange={(event) => setSelectedRace(event.target.value)}
+                label="Race"
+              >
+                {races.map((item, idx: number) => (
+                  <MenuItem key={item.name + idx} value={item.name}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            {!selectedRace && <br />}
+            {selectedRace && (
+              <Typography>
+                {find(races, { name: selectedRace })?.description}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+              <InputLabel id="demo-simple-select-standard-label">
+                Starting Location
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={selectedLocation}
+                onChange={(event) => setSelectedLocation(event.target.value)}
+                label="Starting Location"
+              >
+                {cities.map((item, idx: number) => (
+                  <MenuItem key={item.name + idx} value={item._id}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            {!selectedLocation && <br />}
+            {selectedLocation && (
+              <Typography>
+                <b>Population:</b>{" "}
+                {find(cities, { _id: selectedLocation })?.population}
+              </Typography>
+            )}
+          </Grid>
+          <Button variant="contained" sx={{ mt: 1 }}>
+            Save
+          </Button>
         </Grid>
       )}
     </>
