c6e6ff3080530ea17d3fef6cd98622d859bc58a7
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2023-12-20 21:29:56 -0600

    config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
+};
