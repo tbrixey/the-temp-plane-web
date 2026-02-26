8e697f558ae49d704262d49600f3f4dee6e40079
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2026-02-26 01:10:55 -0600

    update urls

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ const isDev = import.meta.env.DEV;
 
 export const config = {
   callbackUrl: isDev
-    ? "http://localhost:5173/game" // Dev server default port
-    : "https://game.thetemporaryplane.com/game", // Production URL - adjust as needed
+    ? "http://localhost:3000/game" // Dev server default port
+    : "https://ttp.trevorbrixey.com/game", // Production URL - adjust as needed
   apiUrl: isDev
     ? "http://localhost:8080/gameapi/"
-    : "https://api.thetemporaryplane.com/gameapi/", // Production API URL - adjust as needed
+    : "https://ttpapi.trevorbrixey.com/gameapi/", // Production API URL - adjust as needed
 };
